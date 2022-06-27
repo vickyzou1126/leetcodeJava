8cadc8696f2d2297f94fe8df863a67c2f05cefa1
Wan Zou <wanzou1126@gmail.com>
2022-06-27 18:23:46 +1000

    added more solved problems

--- Problem.java
+++ Problem.java
@@ -4,10 +4,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Hashtable;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Problem {
 	public static void main(String[] args) {
-		fourSum(new int[] {1000000000,1000000000,1000000000,1000000000},-294967296);
+		nextPermutation(new int[] {1,3,2});
 	}
 	
 	// 1. Two Sum
@@ -79,7 +80,6 @@ public class Problem {
        }
        return list;
    }
-
     
     // 16. 3Sum Closest
     public static int threeSumClosest(int[] nums, int target) {
@@ -149,5 +149,68 @@ public class Problem {
     	}
     	
         return list;
+    }
+
+    // 26. Remove Duplicates from Sorted Array
+    public int removeDuplicates(int[] nums) {
+    	int index=1;
+    	int val = nums[0];
+    	for(int i=1; i<nums.length;i++) {
+    		if(nums[i]!=val) {
+    			val = nums[i];
+    			nums[index] = val;
+    			index++;
+    		}
+    	}
+        return index;
+    }
+
+    // 27. Remove Element
+    public int removeElement(int[] nums, int val) {
+         int index=0;
+         for(int i=0;i<nums.length;i++) {
+        	 if(nums[i]!=val) {
+        		 nums[index] = nums[i];
+        		 index++;
+        	 }
+         }
+         
+         return index;
+    }
+
+    // 31. Next Permutation
+    public static void nextPermutation(int[] nums) {
+    	Hashtable<Integer, Integer> dictionary = new Hashtable<Integer, Integer>();
+    	
+    	int len = nums.length;
+    	dictionary.put(len-1, nums[len-1]);
+    	
+    	for(int i=len-2;i>=0;i--) {
+    		var currentVal = nums[i];
+    		var values= dictionary.entrySet() 
+    		          .stream() 
+    		          .filter(map -> map.getValue() > currentVal) 
+    		          .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()))
+    		          .values().toArray();
+    		
+    		if (values.length > 0) {
+    			java.util.Arrays.sort(values);
+        		var indexes= dictionary.entrySet() 
+      		          .stream() 
+      		          .filter(map -> map.getValue() == values[0]) 
+      		          .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()))
+  		              .keySet().toArray();
+    			java.util.Arrays.sort(indexes);
+		        int index = (int)indexes[0];
+		        int val = nums[index];
+    			nums[index] = nums[i];
+    			nums[i]=val;
+    			java.util.Arrays.sort(nums,i+1, len);
+    			return;
+    		}
+    		dictionary.put(i, nums[i]);
+    		
+    	}
+    	java.util.Arrays.sort(nums);
     }
 }
