85d1b67e5af3c109414a600208dd4097816faeb5
Wan Zou <wanzou1126@gmail.com>
2022-07-22 17:57:37 +1000

    added more problems

--- Problem.java
+++ Problem.java
@@ -1424,5 +1424,73 @@ public class Problem {
     		 }
     	 }
      }
+
+     // 204. Count Primes
+     public int countPrimes(int n) {
+    	 if(n<=1) return 0;
+    	 
+         int[] res = new int[n];
+         for(int i=2;i<n;i++) {
+        	 res[i] = 1;
+         }
+         for(int i=2;i<n;i++) {
+        	 if(res[i]==0) continue;
+        	 int counter=2;
+        	 while(counter*i<n) {
+        		 res[i*counter] = 0;
+        		 counter++;
+        	 }
+         }
+         
+         return Arrays.stream(res).filter(x -> x == 1).toArray().length;
+     }
+
+     // 209. Minimum Size Subarray Sum
+     public int minSubArrayLen(int target, int[] nums) {
+    	 int len = nums.length;
+         if (nums[0] >= target) return 1;
+     	 int res = len+1;
+     	 int sum = nums[0];
+     	 
+     	 int left =0;
+     	 int right =0;
+     	 for(int i=1; i<len; i++) {
+     		 if(nums[i]>=target) return 1;
+     		 right = i;
+     		 sum+=nums[i];
+     		 if (sum>=target) {
+     			 while(sum>=target && left<right) {
+     				 sum-=nums[left];
+     				 left++;
+     			 }
+     			 res = Math.min(res, right-left+2);
+     		 }
+     	 }
+     	
+     	 return res == len+1 ? 0 : res;
+     }
+
+     // 213. House Robber II
+     public int rob2(int[] nums) {
+    	 int len = nums.length;
+    	 if(len==1) return nums[0];
+         int[] dp = new int[len];
+         // from 1
+         dp[0] = nums[0];
+         dp[1] = nums[0];
+         for(int i=2;i<len-1;i++) {
+        	 dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i]);
+         }
+         
+         int[] dp2 = new int[len];
+         // from 1
+         dp2[0] = 0;
+         dp2[1] = nums[1];
+         for(int i=2;i<len;i++) {
+        	 dp2[i] = Math.max(dp2[i-1], dp2[i-2]+nums[i]);
+         }
+         
+         return Math.max(dp[len-2], dp2[len-1]);
+     }
 }
 
